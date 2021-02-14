93941a1de4b2bf316bc4e8a854bbf04215ba029f
Vincenzo Marcovecchio <vinceoldmark@gmail.com>
2021-02-14 17:23:15 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -1,16 +1,3 @@
 const optimizedImages = require('next-optimized-images');
 
 module.exports = optimizedImages;
-
-module.exports = {
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: 'empty',
-      };
-    }
-
-    return config;
-  },
-};
