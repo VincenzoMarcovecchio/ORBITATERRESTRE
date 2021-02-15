a9198fe1ab1229d72f2bac59a8867bfc7b8d3506
Vincenzo Marcovecchio <vinceoldmark@gmail.com>
2021-02-15 19:58:05 +0100

    tomorrow

--- pages/mappa-segnalazioni-ufo-abruzzo.js
+++ pages/mappa-segnalazioni-ufo-abruzzo.js
@@ -2,23 +2,67 @@ import React, { useState, useEffect, useMemo } from 'react';
 import Link from 'next/link';
 import { LayoutComponent, Bio, SEO } from '@components/common';
 import styles from '../assets/Home.module.css';
-import dynamic from 'next/dynamic';
+import useSWR from 'swr';
+import { addDataLayer } from '../map/addDataLayer';
+import { initializeMap } from '../map/initializeMap';
+import { fetcher } from '../utils/fetcher';
+import Head from 'next/head';
+const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
-const DEFAULT_CENTER = [38.907132, -77.036546];
 export default function Home() {
-  const MapWithNoSSR = useMemo(() =>
-    dynamic(() => import('../Map/Map.js'), {
-      loading: () => <p>A map is loading</p>,
-      ssr: false,
-    })
-  );
-  return (
-    <LayoutComponent>
-      <SEO title="Mappa segnalazioni UFO in Abruzzo" />
+  const [pageIsMounted, setPageIsMounted] = useState(false);
+  const [Map, setMap] = useState();
+  const { data, error } = useSWR('/api/liveMusic', fetcher);
 
-      <div className="z-10 max-w-7xl mx-auto px-4 sm:px-6 max-w-screen-lg mx-auto ">
-        <MapWithNoSSR />
-      </div>
-    </LayoutComponent>
+  if (error) {
+    console.error(error);
+  }
+
+  mapboxgl.accessToken =
+    'pk.eyJ1Ijoid2FubmFkYyIsImEiOiJjazBja2M1ZzYwM2lnM2dvM3o1bmF1dmV6In0.50nuNnApjrJYkMfR2AUpXA';
+
+  useEffect(() => {
+    setPageIsMounted(true);
+
+    let map = new mapboxgl.Map({
+      container: 'my-map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-77.02, 38.887],
+      zoom: 12.5,
+      pitch: 45,
+      maxBounds: [
+        [-77.875588, 38.50705], // Southwest coordinates
+        [-76.15381, 39.548764], // Northeast coordinates
+      ],
+    });
+
+    initializeMap(mapboxgl, map);
+    setMap(map);
+  }, []);
+
+  useEffect(() => {
+    if (pageIsMounted && data) {
+      Map.on('load', function () {
+        addDataLayer(Map, data);
+      });
+    }
+  }, [pageIsMounted, setMap, data, Map]);
+
+  return (
+    <>
+      <Head>
+        <link
+          href="https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css"
+          rel="stylesheet"
+        />
+      </Head>
+      <LayoutComponent>
+        <SEO title="Mappa segnalazioni UFO in Abruzzo" />
+
+        <div className="z-10 max-w-7xl mx-auto px-4 sm:px-6 max-w-screen-lg mx-auto ">
+          <div id="my-map" style={{ height: 500, width: '100%' }} />
+        </div>
+      </LayoutComponent>
+    </>
   );
 }
