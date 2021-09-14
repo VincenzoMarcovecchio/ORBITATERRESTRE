e50c877066950634504d745ddf5120bfab51b7ba
unknown <vinceoldmark@gmail.com>
2021-09-14 12:40:56 +0100

    change

--- pages/mercurio-in-retrogrado-previsioni-astronomiche-in-tempo-reale.js
+++ pages/mercurio-in-retrogrado-previsioni-astronomiche-in-tempo-reale.js
@@ -124,7 +124,7 @@ function Retrogrado() {
               </a>
             </p>
           </form>
-          {defaultResults.length  && (
+          {defaultResults !== null  && (
             <h3 className="text-3xl font-bold text-yellow-600 font-display mt-8 mx-auto mb-6">
               {defaultResults.is_retrograde === false ? "FALSO" : "VERO"}
             </h3>
