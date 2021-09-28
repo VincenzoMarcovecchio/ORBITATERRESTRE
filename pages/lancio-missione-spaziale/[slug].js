8f6c9d1ea7fe5ff40f5bd1c5305bc1b193735c73
unknown <vinceoldmark@gmail.com>
2021-09-28 12:17:30 +0100

    wut

--- pages/lancio-missione-spaziale/[slug].js
+++ pages/lancio-missione-spaziale/[slug].js
@@ -9,7 +9,7 @@ function SingoloLancio({ newsar }) {
       <SEO
         title={`${newsar.results[0]?.name}`}
         cano="si"
-        slug={`lancio-missione-spazial/${newsar.results[0]?.slug}`}
+        slug={`lancio-missione-spaziale/${newsar.results[0]?.slug}`}
         imageUrl={newsar.results[0]?.image}
         description={`${newsar.results[0]?.mission.description}`}
       />
@@ -17,40 +17,40 @@ function SingoloLancio({ newsar }) {
       <div className=" max-w-7xl mx-auto px-4 sm:px-6 sm:px-6 display flex flex-col items-start">
         {newsar.results.length ? (
           <article className="sm:grid md:flex sm:flex-col md:flex-row max-w-full  mt-6 mb-8 shadow-lg rounded-lg overflow-hidden">
-            {newsar.results[0].image ? (
+            {newsar.results[0]?.image ? (
               <img
                 className="sm:w-full md:w-1/3 object-cover"
                 S
-                src={newsar.results[0].image}
-                alt={newsar.results[0].name}
+                src={newsar.results[0]?.image}
+                alt={newsar.results[0]?.name}
               />
             ) : (
               <img
                 className="sm:w-full md:w-1/3 object-cover"
                 src={non}
-                alt={newsar.results[0].name}
+                alt={"immagine non trovata"}
               />
             )}
 
             <div className="sm:w-full md:w-2/3 px-4  py-6 ">
               <h1 className="mt-8 mb-8 text-4xl font-bold text-yellow-600 font-display">
-                Lancio {newsar.results[0].name}
+                Lancio {newsar.results[0]?.name}
               </h1>
               <div className="flex">
                 <b>Status:</b>&nbsp;
-                <p className="">{newsar.results[0].status.abbrev}</p>
+                <p className="">{newsar.results[0]?.status.abbrev}</p>
               </div>
               <div className="flex">
                 <b>Descrizione:</b>&nbsp;
-                <p className="">{newsar.results[0].status.description}</p>
+                <p className="">{newsar.results[0]?.status.description}</p>
               </div>
               <div className="flex">
                 <b>Piattaforma di lancio:</b>&nbsp;
-                <p className=""> {newsar.results[0].pad.name}</p>
+                <p className=""> {newsar.results[0]?.pad.name}</p>
               </div>
               <div className="flex">
                 <b>Location:</b>&nbsp;
-                <p>{newsar.results[0].location.name}</p>
+                <p>{newsar.results[0]?.location.name}</p>
               </div>
             </div>
           </article>
