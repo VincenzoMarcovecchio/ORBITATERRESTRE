d4c765cd0036b863498a08984480dc0d3cd297ee
unknown <vinceoldmark@gmail.com>
2021-11-01 20:05:58 +0000

    change

--- components/common/Lanci.js
+++ components/common/Lanci.js
@@ -7,7 +7,7 @@ import { fetcher } from "../../utils/fetcher";
 
 export function Lanci() {
   const url =
-    "https://mimmofranco.herokuapp.com/https://lldev.thespacedevs.com/2.2.0/launch/upcoming/";
+    "https://lldev.thespacedevs.com/2.2.0/launch/upcoming/?limit=10&offset=10";
 
   const { data, error } = useSWR(url, fetcher);
  
