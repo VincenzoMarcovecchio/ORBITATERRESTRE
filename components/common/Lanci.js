f22fd4c159c174eb42eb7097b412e451c45647f8
unknown <vinceoldmark@gmail.com>
2021-12-08 20:30:26 +0000

    change2

--- components/common/Lanci.js
+++ components/common/Lanci.js
@@ -4,73 +4,79 @@ import useSWR from "swr";
 import { Countdown } from "../../utils/countdown";
 import { fetcher } from "../../utils/fetcher";
 
-export  function Lanci() {
+export function Lanci() {
   const url =
     "https://mimmofranco.herokuapp.com/https://lldev.thespacedevs.com/2.2.0/launch/upcoming/?limit=10&offset=10";
 
-  const { data, error } =  useSWR(url,  fetcher);
-  
+  const { data, error } = useSWR(url, fetcher);
+
   return (
     <div className="md:px-4 flex flex-col mio">
       {data && data.results !== undefined ? (
-        data.results.map((la) => {
-          return (
-            <figure
-              className="flex mb-8 flex-col justify-between max-w-72 h-96 bg-white bg-center text-gray-800 shadow-md overflow-hidden cursor-pointer w-full"
-              style={{
-                backgroundImage: `url(${la.image})`,
-                backgroundSize: "cover",
-                backgroundRepeat: "no-repeat",
-              }}
-              key={la.id}
-            >
-              <div className="flex justify-between items-center ml-4 pr-8">
-                <div className="bg-indigo-500 bg-opacity-95 text-grey-darker bg-opacity-95 shadow px-2 py-1 flex items-center font-bold text-xs rounded">
-                  <Countdown
-                    timeTillDate={la.window_end}
-                    timeFormat={"YYYY MM DD, h:mm a"}
-                  />
+        data.results
+          .sort(function (a, b) {
+            return (
+              Number(new Date(a.window_end)) - Number(new Date(b.window_end))
+            );
+          })
+          .map((la) => {
+            return (
+              <figure
+                className="flex mb-8 flex-col justify-between max-w-72 h-96 bg-white bg-center text-gray-800 shadow-md overflow-hidden cursor-pointer w-full"
+                style={{
+                  backgroundImage: `url(${la.image})`,
+                  backgroundSize: "cover",
+                  backgroundRepeat: "no-repeat",
+                }}
+                key={la.id}
+              >
+                <div className="flex justify-between items-center ml-4 pr-8">
+                  <div className="bg-indigo-500 bg-opacity-95 text-grey-darker bg-opacity-95 shadow px-2 py-1 flex items-center font-bold text-xs rounded">
+                    <Countdown
+                      timeTillDate={la.window_end}
+                      timeFormat={"YYYY MM DD, h:mm a"}
+                    />
+                  </div>
+                  <small className="bg-white bg-opacity-95 p-2 w-auto h-auto shadow flex flex-col-reverse text-center font-bold text-red-400 ">
+                    {la.mission?.type || "non specificato"}
+                  </small>
                 </div>
-                <small className="bg-white bg-opacity-95 p-2 w-auto h-auto shadow flex flex-col-reverse text-center font-bold text-red-400 ">
-                  {la.mission?.type || "non specificato"}
-                </small>
-              </div>
 
-              <figcaption className="bg-gray-50  shadow-md rounded-r-xl p-4 flex flex-col mr-4 mb-8">
-                <p>
-                  <b>Agenzia:&nbsp;</b>
-                  <Link
-                    className=" z-20"
-                    replace
-                    href={`/agenzia-spaziale/${la.launch_service_provider.id}/`}
-                  >
-                    <a>{la.launch_service_provider.name}</a>
-                  </Link>
-                </p>
-                <p>
-                  <b>Dove:&nbsp;</b>
-                  <Link
-                    replace
-                    className="truncate text-gray-500 text-sm"
-                    href={`/piattaforma-lancio-pad/${la.pad.id}/`}
-                  >
-                    <a>{la.pad?.name}</a>
-                  </Link>
-                </p>
-                <p>
-                  <b>Nome Missione:&nbsp;</b>
-                  <Link
-                    replace
-                    className="truncate text-gray-500 text-sm"
-                    href={`/lancio-missione-spaziale/${la.id}`}
-                  >
-                    <a>{la.mission?.name}</a>
-                  </Link>
-                </p>
-              </figcaption>
-            </figure>
-          );
-        })
+                <figcaption className="bg-gray-50  shadow-md rounded-r-xl p-4 flex flex-col mr-4 mb-8">
+                  <p>
+                    <b>Agenzia:&nbsp;</b>
+                    <Link
+                      className=" z-20"
+                      replace
+                      href={`/agenzia-spaziale/${la.launch_service_provider.id}/`}
+                    >
+                      <a>{la.launch_service_provider.name}</a>
+                    </Link>
+                  </p>
+                  <p>
+                    <b>Dove:&nbsp;</b>
+                    <Link
+                      replace
+                      className="truncate text-gray-500 text-sm"
+                      href={`/piattaforma-lancio-pad/${la.pad.id}/`}
+                    >
+                      <a>{la.pad?.name}</a>
+                    </Link>
+                  </p>
+                  <p>
+                    <b>Nome Missione:&nbsp;</b>
+                    <Link
+                      replace
+                      className="truncate text-gray-500 text-sm"
+                      href={`/lancio-missione-spaziale/${la.id}`}
+                    >
+                      <a>{la.mission?.name}</a>
+                    </Link>
+                  </p>
+                </figcaption>
+              </figure>
+            );
+          })
       ) : (
         <b>{"caricamento supersonico in corso..."}</b>
       )}
