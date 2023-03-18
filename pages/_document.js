eb9362dceb92d1ac30e5272344d16140c57fbbd6
Vincenzo Marcovecchio <55591572+VincenzoMarcovecchio@users.noreply.github.com>
2023-03-18 10:17:26 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang={siteMetadata.language}>
         <Head>
-      <meta name="google-site-verification" content="4-ituOXVzOpj_KU4wvfnoc3wVaNNkK67J4abuZT4WbY">   
+      <meta name="google-site-verification" content="4-ituOXVzOpj_KU4wvfnoc3wVaNNkK67J4abuZT4WbY"/>   
       </Head>
         <body className="bg-gray-50">
           <Main />
